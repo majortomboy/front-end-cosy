4df09bbc2312d75ac9646b608c72046830f7c375
Sidney DuPont <sidneyadupont@gmail.com>
2021-07-26 14:06:22 -0700

    Lays out blueprint for project dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -27,16 +27,97 @@ function Dashboard() {
     // How am I supposed to use the dependency array here?
 
     return (
-        <div>
-            <h2>Welcome to your project dashboard for {dashboardData.title}!</h2>
-            <div>
-                <h4>Character: {dashboardData.title}</h4>
-                <h4>Series: {dashboardData.series}</h4>
-                <h4>Budget: ${dashboardData.budget}</h4>
-            </div>
-            <Link to="/">
-                <button>Back to Projects</button>
-            </Link>
+        <div className="container-fluid">
+            <div className="row">
+                <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar text-start">
+                    <ul class="nav flex-column my-5">
+                        <li class="nav-item">
+                            <Link class="nav-link active h5" to={`/dashboard/${id}/todo/`}>
+                                <span class="ml-2">To Do List</span>
+                            </Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link h5" to={`/dashboard/${id}/tobuy/`}>
+                                <span class="ml-2">To Buy List</span>
+                            </Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link h5" to={`/dashboard/${id}/references/`}>
+                                <span class="ml-2">Reference Photos</span>
+                            </Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link h5" to={`/dashboard/${id}/resources/`}>
+                                <span class="ml-2">Resources</span>
+                            </Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link h5" to={`/dashboard/${id}/settings/`}>
+                                <span class="ml-2">Settings</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            <main className="col-md-9 ml-sm-auto col-lg-10 px-md-4 py-4">
+                <h1 className="h2 flex-column">Project Dashboard - {dashboardData.title}</h1>
+                <div className="row border border-primary">
+                    <div className="col">
+                        <img className="rounded border border-primary h-50" src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=webp&v=1530129081" alt="placeholder"/>
+                        <div className="row">
+                            <div className="row border border-warning">
+                                <h4>Parts</h4>
+                                <ul className="list-group text-start">
+                                    <li className="list-group-item">
+                                        <input className="form-check-input me-1" type="checkbox" value=""></input>
+                                        First Checkbox
+                                    </li>
+                                    <li className="list-group-item">
+                                        <input className="form-check-input me-1" type="checkbox" value=""></input>
+                                        Second Checkbox
+                                    </li>
+                                    <li className="list-group-item">
+                                        <input className="form-check-input me-1" type="checkbox" value=""></input>
+                                        Third Checkbox
+                                    </li>
+                                    <li className="list-group-item">
+                                        <input className="form-check-input me-1" type="checkbox" value=""></input>
+                                        Fourth Checkbox
+                                    </li>
+                                    <li className="list-group-item">
+                                        <input className="form-check-input me-1" type="checkbox" value=""></input>
+                                        Fifth Checkbox
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col border border-secondary">
+                        <div className="row border border-primary text-start">
+                            <h4>{dashboardData.title}</h4>
+                            <p className=" fst-italic">{dashboardData.series}</p>
+                            <p className="fw-bold">Due Date: {dashboardData.due_date}</p>
+                        </div>
+                        <div className="row my-5 border border-danger">
+                            <div className="col mx-3">
+                                <div className="card">
+                                <h4 className="card-header">Budget</h4>
+                                    <p>${dashboardData.budget}</p>
+                                </div>
+                            </div>
+                            <div className="col mx-3">
+                                <div className="card">
+                                <h4 className="card-header">Completion Status</h4>
+                                <p>0 %</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div>
+                </div>
+            </main>
+        </div>
         </div>
     );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -12,7 +12,7 @@ function Project(props) {
         <div className="col-lg-4">
             <Link className="navbar-brand" to={`/dashboard/${props.project_id}`}>
                 <div className="card">
-                    <img src={props.photo} className="card-img-top" alt=""></img>
+                    <img src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=webp&v=1530129081" alt="placeholder" className="card-img-top"></img>
                     <div className="card-body">
                         <p className="card-text fw-bold">{props.title}</p>
                         <p className="card-text fst-italic">{props.series}</p>
